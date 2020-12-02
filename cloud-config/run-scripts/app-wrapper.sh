9e25306504904ed82d2b9f2ddf9553a7fd740e7d
Paul Sud <paulsud@stanford.edu>
2020-12-02 10:30:27 -0800

    add sleep

--- cloud-config/run-scripts/app-wrapper.sh
+++ cloud-config/run-scripts/app-wrapper.sh
@@ -77,6 +77,7 @@ if [ -z "$ENCD_PG_IP" ]; then
 fi
 $ENCD_SCRIPTS_DIR/app-encd.sh
 sudo -u root $ENCD_SCRIPTS_DIR/app-a2en.sh
+sleep 5m
 if [ "$ENCD_BATCHUPGRADE" == "true" ]; then
     $ENCD_SCRIPTS_DIR/app-batchupgrade.sh production.ini $BUP_VAR_1 $BUP_VAR_2 $BUP_VAR_3 $BUP_VAR_4
 fi
